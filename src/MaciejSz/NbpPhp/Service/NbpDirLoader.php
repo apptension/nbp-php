450282426967035b91d90316b97e631cae316e17
Mateusz Wójcik <mateusz.wojcik@apptension.com>
2018-01-02 14:59:03 +0100

    fix dirname after new year

--- src/MaciejSz/NbpPhp/Service/NbpDirLoader.php
+++ src/MaciejSz/NbpPhp/Service/NbpDirLoader.php
@@ -5,12 +5,7 @@ use MaciejSz\NbpPhp\Exc\ECouldNotLoadDir;
 
 class NbpDirLoader
 {
-    const DIR_URL = 'http://www.nbp.pl/kursy/xml/dir%s.txt';
-
-    /**
-     * @var array|string[]
-     */
-    private static $_url_cache = [];
+    const DIR_URL = 'http://www.nbp.pl/kursy/xml/dir.txt';
 
     /**
      * @param int $year
@@ -18,7 +13,7 @@ class NbpDirLoader
      */
     public function load($year)
     {
-        $url = $this->getUrl($year);
+        $url = self::DIR_URL;
         $dir = [];
         set_error_handler($this->_getLoadStreamErrorHandler($url));
         $txt = file_get_contents($url);
@@ -39,22 +34,6 @@ class NbpDirLoader
         }
         ksort($dir);
         return $dir;
-    }
-
-    /**
-     * @param null|int $year
-     * @return string
-     */
-    public function getUrl($year = null)
-    {
-        $year = substr($year, 0, 4);
-        if ( empty($year) || self::_getCurrentYear() == $year ) {
-            $year = "";
-        }
-        if ( !isset(self::$_url_cache[$year]) ) {
-            self::$_url_cache[$year] = sprintf(self::DIR_URL, $year);
-        }
-        return sprintf(self::DIR_URL, $year);
     }
 
     /**
